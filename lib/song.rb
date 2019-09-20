bf4da720a9b3afc75da8394f8686f82278067c32
Jonah Brown <jswish02@gmail.com>
2019-09-20 20:47:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+
+class Song 
+  attr_accessor :name,  :artists, :genres,
+@@count = 0 
+@@artists = []
+@@genres = []
+
+ def initialize (name, artists, genres)
+ @name = name 
+ @artists = artists
+ @genres = genres 
+ @@count += 1 
+ @@genres << genres 
+ @@artist << artists
+ end 
+ def self.artists
+   @@artist.uniq
+ end 
+ def self.artist_count
+   artist_count = {}
+   @@artist_.each do |genre|
+     if genre_count
